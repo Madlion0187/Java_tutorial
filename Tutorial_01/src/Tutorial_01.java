4ce4a05191bcc8f20e8a69517ac74ce6bb97698a
Norbert_Varga <109357322+Madlion0187@users.noreply.github.com>
2025-05-24 17:39:52 +0200

    this is my switch case

--- Tutorial_01/src/Tutorial_01.java
+++ Tutorial_01/src/Tutorial_01.java
@@ -1,3 +1,4 @@
+
 public class Tutorial_01
 {
     // 1.Task
@@ -7,7 +8,7 @@ public class Tutorial_01
     }
 
     // 2.Task
-    public  static int division (int a, int b)
+    public static int division (int a, int b)
     {
         if (b == 0)
         {
@@ -21,14 +22,49 @@ public class Tutorial_01
     }
 
     // 3.Task
-    public  static int remainder (int a, int b)
+    public static int remainder (int a, int b)
     {
         return b == 0 ? exception() : a % b;
     }
-    public  static int exception ()
+    public static int exception ()
     {
         System.out.println("division by zero is not possible");
         return 0;
+    }
+
+    // 4.Task
+    public static String months (int a)
+    {
+        switch (a)
+        {
+            case 1:
+                return "January";
+            case 2:
+                return "February";
+            case 3:
+                return "March";
+            case 4:
+                return "April";
+            case 5:
+                return "May";
+            case 6:
+                return "June";
+            case 7:
+                return "July";
+            case 8:
+                return "August";
+            case 9:
+                return "September";
+            case 10:
+                return "October";
+            case 11:
+                return "November";
+            case 12:
+                return "December";
+            default:
+                return "There is no such month";
+        }
+
     }
 
     public static void main(String[] args)
@@ -36,6 +72,7 @@ public class Tutorial_01
         System.out.println(addition(5, 6));
         System.out.println(division(25, 5));
         System.out.println(remainder(25 ,0));
+        System.out.println(months(11));
 
     }
 }
